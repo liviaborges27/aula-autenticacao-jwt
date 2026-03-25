746cce2fb82790ae517f319c7d65ac3617f3dd0b
Lívia Borges Matos <livia.matos2721@gmail.com>
2026-03-25 16:33:06 -0300

    feat: Implentando autenticação na API
    Criando proteção de rotas através de verificação de tokens.

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,15 +1,19 @@
 import express, { type Request, type Response } from 'express';
 import PessoaController from './controller/PessoaController.js';
+import { Auth } from './middleware/Auth.js';
 
 const router = express.Router();
 
+
 router.get('/', (req: Request, res: Response) => { res.send('Hello World!') });
 
-router.get('/api/protegido', 
-        (req: Request, res: Response) => 
-            { res.send('Rota protegida, se você está vendo essa mensagem é porque está autenticado no sistema') }
-    );
 
-router.get('/api/pessoas', PessoaController.todos);
+
+
+router.get('/api/protegido', Auth.verifyToken, (req: Request, res: Response) => { res.send('Rota protegida, se você está vendo essa mensagem é porque está autenticado no sistema') });
+
+router.get('/api/pessoas', Auth.verifyToken, PessoaController.todos);
+
+router.post('/api/login', Auth.validacaoUsuario);
 
 export { router }
